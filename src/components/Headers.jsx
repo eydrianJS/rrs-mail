5409e79ac9f4d1c979709a7c613c50a97d705af5
Adrian Olszowski <aol@spyro-soft.com>
2021-02-21 13:53:10 +0100

    init

---
+++ src/components/Headers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppleIcon from '@material-ui/icons/Apple';
+
+const Headers = () => {
+  return (
+    <div>
+      <AppleIcon />
+    </div>
+  );
+};
+
+export default Headers;
